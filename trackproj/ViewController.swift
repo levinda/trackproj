5e46a100020d0057632c818f63f6893ed2b9c10d
levinda <danil.levin@gmail.com>
2019-09-27 23:17:04 +0300

    Initial_Project_Commit

---
+++ trackproj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trackproj
+//
+//  Created by Danil on 27/09/2019.
+//  Copyright © 2019 levinda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ trackprojTests/trackprojTests.swift
@@ -0,0 +1,34 @@
+//
+//  trackprojTests.swift
+//  trackprojTests
+//
+//  Created by Danil on 27/09/2019.
+//  Copyright © 2019 levinda. All rights reserved.
+//
+
+import XCTest
+@testable import trackproj
+
+class trackprojTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
